c24b5f64e10cb1ae728ec766d27f7714c2121a36
Skullmasher <skullmasher@heartlessgaming.com>
2014-11-04 00:58:08 +0100

    Moving navbar above the site branding

--- wp-content/themes/heartless/functions.php
+++ wp-content/themes/heartless/functions.php
@@ -75,18 +75,18 @@ add_action( 'after_setup_theme', 'heartless_setup' );
  *
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
-function heartless_widgets_init() {
-	// register_sidebar( array(
-	// 	'name'          => __( 'Sidebar', 'heartless' ),
-	// 	'id'            => 'sidebar-1',
-	// 	'description'   => '',
-	// 	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-	// 	'after_widget'  => '</aside>',
-	// 	'before_title'  => '<h1 class="widget-title">',
-	// 	'after_title'   => '</h1>',
-	// ) );
-}
-add_action( 'widgets_init', 'heartless_widgets_init' );
+// function heartless_widgets_init() {
+// 	register_sidebar( array(
+// 		'name'          => __( 'Sidebar', 'heartless' ),
+// 		'id'            => 'sidebar-1',
+// 		'description'   => '',
+// 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+// 		'after_widget'  => '</aside>',
+// 		'before_title'  => '<h1 class="widget-title">',
+// 		'after_title'   => '</h1>',
+// 	) );
+// }
+// add_action( 'widgets_init', 'heartless_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
